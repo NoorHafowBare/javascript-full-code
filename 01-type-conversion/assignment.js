e3d9524a5cd7194611d589810166f968ea391fa6
noor <noorhafowbare@gmail.com>
2025-03-21 15:11:41 +0300

    initial commit

---
+++ 01-type-conversion/assignment.js
@@ -0,0 +1,10 @@
+// type converstion
+let money = "50";
+console.log(money);
+
+// convert string to number
+money = parseInt(money);
+
+console.log(typeof money);
+
+// convert number to string
---
+++ 02-switch-statements/index.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("a is less than b");
+} else {
+  console.log("a is eaqula to b");
+}
+
+let time = 10;
+let greetings;
+
+if (time < 10) {
+  greetings = "good morning";
+  console.log(greetings);
+} else if (time < 20) {
+  greetings = "good day";
+  console.log(greetings);
+} else {
+  greetings = "good evening";
+  console.log(greetings);
+}
